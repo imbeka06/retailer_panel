9a062713a884131c46619a7413638e16b49468b0
imbeka06 <146805644+imbeka06@users.noreply.github.com>
2024-03-22 23:17:45 +0300

    Add files via upload

---
+++ util.types.js
@@ -0,0 +1,60 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * query operations:
+ * 1.where clause
+ * 2.include clause
+ * 3.select clause
+ *
+ *
+ * Model Queries
+ * 1.create
+ * 2.createMany
+ * 3.update
+ * 4.updateMany
+ * 5.findMany
+ * 6.findFirst
+ * 7.findUnique
+ * 8.delete
+ * 9.deleteMany
+ * 10.count
+ * 11.aggregate
+ *      a).where
+ *      b).cursor
+ *      c).take
+ *      d).skip
+ *      e)._count => non-nullable values
+ *      f)._avg => average values
+ *      g)._sum => sum of all specified values
+ *      h)._min => minimum values of the specified values
+ *      i)._max => maximum values of the specifies values
+ *
+ * 12.groupBy
+ *      a).where
+ *      b).orderBy => order list values present in the by field
+ *      c).by => specifies the field or combination of field to group by records
+ *      d).having => filter groups by an aggregrate value
+ * 13.upsert
+ * 14.connect
+ * 15.connectOrCreate
+ * 16.take
+ * 17.skip
+ * 18.
+ *
+ * operators
+ * AND
+ * OR
+ * some
+ * any
+ * every
+ * is
+ * isNot
+ * gt
+ * gte
+ * lt
+ * lte
+ * endsWith
+ * startsWith
+ * contains
+ *
+ */ 
